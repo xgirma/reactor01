4a241f6ff8eef2ba05047b1d45307102e29eff1e
Girma Nigusse <xgirma@gmail.com>
2017-11-16 02:32:47 -0800

    var vs let vs const

---
+++ 01/06.hoisting.js
@@ -0,0 +1,27 @@
+function discountedPrices (prices, discount) {
+    var i;
+    var discountedPrice;
+    var finalPrice;
+
+
+    discounted = [];
+    for(i = 0; i < prices.length; i++){
+        discountedPrice = prices[i] * (1 - discount);
+        finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(finalPrice);
+    }
+
+    console.log('i: ', i);
+    console.log('discountedPrice: ', discountedPrice);
+    console.log('finalPrice: ', finalPrice);
+
+    return discounted;
+}
+
+console.log(discountedPrices([100,200,300], .5));
+console.log('last: ', discounted);
+// i:  3
+// discountedPrice:  150
+// finalPrice:  150
+// [ 50, 100, 150 ]
+// last:  [ 50, 100, 150 ]
---
+++ 01/09.const.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Tome'
+};
+console.log(person);
+person.name = 'Tyler';
+console.log(person);
+
+// { name: 'Tome' }
+// { name: 'Tyler' }
